53362cc85d83d418c55f1d00ffcf4b67eda5f587
Thomas Wang <thomaswang@berkeley.edu>
2022-09-20 01:04:48 -0700

    edit

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ SIGNING_SECRET = os.environ.get('SLACK_SIGNING_SECRET')
 
 app = Flask(__name__)
 
-slack_event_adapter = SlackEventAdapter(SIGNING_SECRET, '/slack/events', app)
+# slack_event_adapter = SlackEventAdapter(SIGNING_SECRET, '/slack/events', app)
 client = slack.WebClient(token = SLACK_TOKEN)
 # client.chat_postMessage(channel='#diversaspotting',text='Get ready slayers.')
 
