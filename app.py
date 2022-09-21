bb810190a9250b8cd5005f10f67af47da2ef800c
Thomas Wang <thomaswang@berkeley.edu>
2022-09-21 08:58:58 -0700

    Added health check

--- app.py
+++ app.py
@@ -4,8 +4,17 @@ from slack_bolt import App
 
 load_dotenv('.env')
 
-app = App(token = os.environ.get('SLACK_AUTH_TOKEN'), signing_secret = os.environ.get('SLACK_SIGNING_SECRET'))
+app = App(
+    token = os.environ.get('SLACK_AUTH_TOKEN'),
+    signing_secret = os.environ.get('SLACK_SIGNING_SECRET')
+)
 
+
+# Health Check
+@app.message("diversabot health check")
+def message_hello(say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"I'm all up and running! Otherwise, let Tommy know.")
 
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
