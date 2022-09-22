6560d6cc657d2daeadf728e4a4661a7b3b4cd0a5
Thomas Wang <thomaswang@berkeley.edu>
2022-09-21 19:59:42 -0700

    pog

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from slack_bolt import App
 import pygsheets
 import pandas as pd
+import re
 
 
 """
@@ -18,35 +19,37 @@ app = App(
     signing_secret = os.environ.get('SLACK_SIGNING_SECRET')
 )
 
-# g_client = pygsheets.authorize(service_file='C:\\Users\\tommy\\Desktop\DiversaBot\\diversabot-363300-f4b5abc8a0c8.json')
+g_client = pygsheets.authorize(service_file='C:\\Users\\tommy\\Desktop\DiversaBot\\diversabot-363300-f4b5abc8a0c8.json')
 
-# sh = g_client.open('DiversaBot')
+sh = g_client.open('DiversaBot')
 
-# sh_spot_history = sh[0]
-# df_spot_history = sh_spot_history.get_as_df(
-#     has_header=True,
-#     index_column=None,
-#     nuumerize=True,
-#     include_tailing_empty=False,
-#     include_tailing_empty_rows=False
-# )
+sh_spot_history = sh[0]
+df_spot_history = sh_spot_history.get_as_df(
+    has_header=True,
+    index_column=None,
+    nuumerize=True,
+    include_tailing_empty=False,
+    include_tailing_empty_rows=False
+)
 
-# """
+"""
 
-# Helper Functions
+Helper Functions
 
-# """
+"""
 
-# def save_spot_history():
-#     sh_spot_history.set_dataframe(
-#         df=df_spot_history,
-#         start=(1,1),
-#         copy_head=True,
-#         extend=True,
-#         copy_index=False
-#     )
+def save_spot_history():
+    sh_spot_history.set_dataframe(
+        df=df_spot_history,
+        start=(1,1),
+        copy_head=True,
+        extend=True,
+        copy_index=False
+    )
 
-
+def find_all_mentions(msg: str) -> list:
+    member_ids = re.findall(r'<@([\w]+)>', msg, re.MULTILINE)
+    return member_ids
 
 """
 
@@ -55,14 +58,17 @@ Routes
 """
 
 
-
+@app.message()
+def record_spot(message, say):
+    say("pog")
+    
 
 # Health Check
 @app.message("diversabot health check")
 def message_hello(message, say):
     # say() sends a message to the channel where the event was triggered
     msg = message['text']
-    print(msg)
+    print(message)
     say(f"I'm all up and running! Otherwise, let Tommy know.")
 
 if __name__ == "__main__":
