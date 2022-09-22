574d8b33786e30be53eab8a840fa5031bfd0e9ac
Thomas Wang <thomaswang@berkeley.edu>
2022-09-21 18:41:22 -0700

    Added google sheets api

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import os
 from dotenv import load_dotenv
 from slack_bolt import App
+import pygsheets
+import pandas as pd
+
+
+"""
+
+Environment Setup
+
+"""
 
 load_dotenv('.env')
 
@@ -9,11 +18,51 @@ app = App(
     signing_secret = os.environ.get('SLACK_SIGNING_SECRET')
 )
 
+g_client = pygsheets.authorize(service_account_env_var = 'GDRIVE_API_CREDENTIALS')
+
+sh = g_client.open('DiversaBot')
+
+sh_spot_history = sh.spotHistory
+df_spot_history = sh_spot_history.get_as_df(
+    has_header=True,
+    index_column=None,
+    nuumerize=True,
+    include_tailing_empty=False,
+    include_tailing_empty_rows=False
+)
+
+"""
+
+Helper Functions
+
+"""
+
+def save_spot_history():
+    sh_spot_history.set_dataframe(
+        df=df_spot_history,
+        start=(1,1),
+        copy_head=True,
+        extend=True,
+        copy_index=False
+    )
+
+
+
+"""
+
+Routes
+
+"""
+
+
+
 
 # Health Check
 @app.message("diversabot health check")
-def message_hello(say):
+def message_hello(message, say):
     # say() sends a message to the channel where the event was triggered
+    msg = message['text']
+    print(msg)
     say(f"I'm all up and running! Otherwise, let Tommy know.")
 
 if __name__ == "__main__":
